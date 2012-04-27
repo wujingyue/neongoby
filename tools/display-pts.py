346bf77bcda0f4723c774d4e8beb263ba3ea7566
Jingyue Wu <jingyue@cs.columbia.edu>
2012-04-26 23:54:34 -0400

    able to print dsaa graph as well

--- tools/display-pts.py
+++ tools/display-pts.py
@@ -3,10 +3,11 @@
 import argparse, os, sys
 
 def get_base_cmd():
-    base_cmd = "opt "
+    base_cmd = "opt -debug "
     base_cmd += "-load $LLVM_ROOT/install/lib/id.so "
     base_cmd += "-load $LLVM_ROOT/install/lib/PointerAnalysis.so "
     base_cmd += "-load $LLVM_ROOT/install/lib/DynamicPointerAnalysis.so "
+    base_cmd += "-load $LLVM_ROOT/install/lib/LLVMDataStructure.so "
     return base_cmd
 
 if __name__ == "__main__":
@@ -16,13 +17,26 @@ if __name__ == "__main__":
     parser.add_argument("log", help = "the point-to log")
     parser.add_argument("-o", metavar = "output",
             help = "the output .dot file", required = True)
+    parser.add_argument("--dsaa",
+            help = "use DSAA as a reference",
+            action = "store_true")
     args = parser.parse_args()
 
     cmd = get_base_cmd()
-    cmd += "-dyn-pa "
-    cmd += "-draw-point-to "
-    cmd += "-log-file " + args.log + " "
+    if args.dsaa:
+        # Use DSAA
+        cmd += "-tbaa "
+        cmd += "-basicaa "
+        cmd += "-ds-aa "
+        cmd += "-basic-pa "
+        cmd += "-draw-point-to "
+    else:
+        # Use DynamicPointerAnalysis
+        cmd += "-dyn-pa "
+        cmd += "-draw-point-to "
+        cmd += "-log-file " + args.log + " "
     cmd += "-dot " + args.o + " "
+    cmd += "-pointer-stats "
     cmd += "-disable-output "
     cmd += "< " + args.bc
 
