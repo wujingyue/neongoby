4570452fa9c46711969747a366b3e99367722c8b
Jingyue Wu <jingyue@cs.columbia.edu>
2012-05-06 16:57:26 -0400

    New features:
    Made TestFP2 even simpler. The old AndersensAA still failed on it.

--- test/Original/TestFP2.c
+++ test/Original/TestFP2.c
@@ -5,30 +5,17 @@
 
 typedef void *(*ThreadFuncType)(void *);
 
-ThreadFuncType thr_funcs[MAX_N_THREADS];
-pthread_t children[MAX_N_THREADS];
+ThreadFuncType thr_func;
 
-void *worker_1(void *arg) {
-  return NULL;
-}
-
-void *worker_2(void *arg) {
+void *worker(void *arg) {
   return NULL;
 }
 
 int main(int argc, char *argv[]) {
-  size_t n_threads = 0;
-  thr_funcs[n_threads++] = worker_1;
-  thr_funcs[n_threads++] = worker_2;
-
-  for (size_t i = 0; i < n_threads; ++i) {
-    pthread_t child;
-    pthread_create(&child, NULL, thr_funcs[i], NULL);
-    children[i] = child;
-  }
-
-  for (size_t i = 0; i < n_threads; ++i)
-    pthread_join(children[i], NULL);
+  pthread_t child;
+  thr_func = worker;
+  pthread_create(&child, NULL, thr_func, NULL);
+  pthread_join(child, NULL);
 
   return 0;
 }
