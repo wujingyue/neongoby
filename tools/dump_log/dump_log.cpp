319123dc2b2fe43cad3655b772b7e8a3bbaa35c1
Jingyue Wu <wujingyue@gmail.com>
2012-10-11 18:58:26 -0400

    Refactor: Reuse LogProcessor in dump_log

--- tools/dump_log/dump_log.cpp
+++ tools/dump_log/dump_log.cpp
@@ -4,55 +4,39 @@
 
 #include <cstdio>
 #include <cassert>
-using namespace std;
 
-#include "dyn-aa/LogRecord.h"
+#include "llvm/Support/CommandLine.h"
+
+#include "dyn-aa/LogProcessor.h"
+
+using namespace std;
+using namespace llvm;
 using namespace dyn_aa;
 
-static void PrintAddrTakenDecl(
-    const AddrTakenDeclLogRecord &Record) {
+namespace dyn_aa {
+struct LogDumper: public LogProcessor {
+  virtual void processAddrTakenDecl(const AddrTakenDeclLogRecord &);
+  virtual void processTopLevelPointTo(const TopLevelPointToLogRecord &);
+  virtual void processAddrTakenPointTo(const AddrTakenPointToLogRecord &);
+};
+}
+
+void LogDumper::processAddrTakenDecl(const AddrTakenDeclLogRecord &Record) {
   printf("%u: %p, %lu\n", Record.AllocatedBy, Record.Address, Record.Bound);
 }
 
-static void PrintTopLevelPointTo(const TopLevelPointToLogRecord &Record) {
+void LogDumper::processTopLevelPointTo(const TopLevelPointToLogRecord &Record) {
   printf("%u => %p\n", Record.PointerValueID, Record.PointeeAddress);
 }
 
-static void PrintAddrTakenPointTo(const AddrTakenPointToLogRecord &Record) {
+void LogDumper::processAddrTakenPointTo(
+    const AddrTakenPointToLogRecord &Record) {
   printf("%p => %p\n", Record.PointerAddress, Record.PointeeAddress);
 }
 
 int main(int argc, char *argv[]) {
-  LogRecordType RecordType;
-  while (fread(&RecordType, sizeof RecordType, 1, stdin) == 1) {
-    switch (RecordType) {
-      case AddrTakenDecl:
-        {
-          AddrTakenDeclLogRecord Record;
-          size_t R = fread(&Record, sizeof Record, 1, stdin);
-          assert(R == 1);
-          PrintAddrTakenDecl(Record);
-        }
-        break;
-      case TopLevelPointTo:
-        {
-          TopLevelPointToLogRecord Record;
-          size_t R = fread(&Record, sizeof Record, 1, stdin);
-          assert(R == 1);
-          PrintTopLevelPointTo(Record);
-        }
-        break;
-      case AddrTakenPointTo:
-        {
-          AddrTakenPointToLogRecord Record;
-          size_t R = fread(&Record, sizeof Record, 1, stdin);
-          assert(R == 1);
-          PrintAddrTakenPointTo(Record);
-        }
-        break;
-      default:
-        assert(false && "Unknown record type");
-    }
-  }
+  cl::ParseCommandLineOptions(argc, argv, "Dumps point-to logs");
+  LogDumper LD;
+  LD.processLog();
   return 0;
 }
--- tools/dynaa_utils.py
+++ tools/dynaa_utils.py
@@ -4,7 +4,6 @@ import rcs_utils
 
 def load_all_plugins(cmd):
     cmd = rcs_utils.load_all_plugins(cmd)
-    cmd = rcs_utils.load_plugin(cmd, 'DynAAUtils')
     cmd = rcs_utils.load_plugin(cmd, 'DynAAAnalyses')
     cmd = rcs_utils.load_plugin(cmd, 'DynAACheckers')
     cmd = rcs_utils.load_plugin(cmd, 'DynAAInstrumenters')
