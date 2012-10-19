93d42bb32d194dafdbb0dfe2203f579834995130
Jingyue Wu <jingyue@cs.columbia.edu>
2012-10-19 11:56:04 -0400

    Added option disable-inline and disable-opt
    disable-inline disables inlining alias checks
    
    disable-opt disables running standard compiler optimizations (-O3)

--- tools/dynaa_insert_alias_checker.py
+++ tools/dynaa_insert_alias_checker.py
@@ -3,7 +3,6 @@
 # Author: Jingyue
 
 import argparse
-import string
 import dynaa_utils
 import rcs_utils
 import sys
@@ -24,17 +23,23 @@ if __name__ == '__main__':
                                 str(dynaa_utils.get_aa_choices()),
                         metavar = 'baseline_aa',
                         choices = dynaa_utils.get_aa_choices())
-    parser.add_argument('--inline',
-                        help = 'whether to inline AssertNoAlias',
+    parser.add_argument('--disable-inline',
+                        help = 'do not inline the alias checks',
                         action = 'store_true',
+                        default = False)
+    parser.add_argument('--disable-opt',
+                        help = 'do not run standard compiler optimization',
+                        action = 'stroe_true',
                         default = False)
     args = parser.parse_args()
 
+    # Initialize output file names after each stage.
     bc_orig = args.prog + '.bc'
-    bc_with_alias_checker_no_opt = args.prog + '.alias_checker_no_opt.bc'
-    bc_with_alias_checker = args.prog + '.alias_checker.bc'
-    bc_inlined = args.prog + '.inlined.bc'
+    bc_ac = args.prog + '.ac.bc'
+    bc_ac_opt = args.prog + '.ac.opt.bc'
+    bc_ac_opt_inline = args.prog + '.ac.opt.inline.bc'
 
+    # Insert alias checks.
     cmd = dynaa_utils.load_all_plugins('opt')
     if args.baseline is None:
         cmd = dynaa_utils.load_aa(cmd, args.aa)
@@ -47,38 +52,36 @@ if __name__ == '__main__':
             sys.exit(1)
         # baseline need be put before aa
         cmd = dynaa_utils.load_aa(cmd, args.baseline, args.aa)
-    cmd = string.join((cmd, '-instrument-alias-checker'))
+    cmd = ' '.join((cmd, '-instrument-alias-checker'))
     if args.max_alias_checks is not None:
-        cmd = string.join((cmd, '-max-alias-checks', str(args.max_alias_checks)))
-    cmd = string.join((cmd,
-                       '-o', bc_with_alias_checker_no_opt,
-                       '<', bc_orig))
+        cmd = ' '.join((cmd, '-max-alias-checks', str(args.max_alias_checks)))
+    cmd = ' '.join((cmd, '-o', bc_ac, '<', bc_orig))
     rcs_utils.invoke(cmd)
 
+    # Run standard optimizations.
     # Don't mix -O3 with the previous command line, because the previous command
     # line may not be using basicaa which can potentially make -O3 pretty slow.
-    cmd = string.join(('opt', '-O3',
-                       '-o', bc_with_alias_checker,
-                       '<', bc_with_alias_checker_no_opt))
+    if args.disable_opt:
+        cmd = ' '.join(('cp', bc_ac, bc_ac_opt))
+    else:
+        cmd = ' '.join(('opt', '-O3', '-o', bc_ac_opt, '<', bc_ac))
     rcs_utils.invoke(cmd)
 
-    if args.inline:
-        cmd = dynaa_utils.load_all_plugins('opt')
-        cmd = string.join((cmd, '-inline-alias-checker'))
-        cmd = string.join((cmd,
-                           '-o', bc_inlined,
-                           '<', bc_with_alias_checker))
-        rcs_utils.invoke(cmd)
-
-    cmd = 'clang++'
-    if args.inline:
-        cmd = string.join((cmd, bc_inlined))
-        cmd = string.join((cmd, '-o', bc_inlined[0:-3]))
+    # Inline alias checks.
+    if args.disable_inline:
+        cmd = ' '.join(('cp', bc_ac_opt, bc_ac_opt_inline))
     else:
-        cmd = string.join((cmd,
-                           bc_with_alias_checker,
-                           rcs_utils.get_libdir() + '/libDynAAAliasChecker.a'))
-        cmd = string.join((cmd, '-o', bc_with_alias_checker[0:-3]))
+        cmd = dynaa_utils.load_all_plugins('opt')
+        cmd = ' '.join((cmd, '-inline-alias-checker'))
+        cmd = ' '.join((cmd, '-o', bc_ac_opt_inline, '<', bc_ac_opt))
+    rcs_utils.invoke(cmd)
+
+    # Codegen.
+    cmd = ' '.join(('clang++', bc_ac_opt_inline))
+    if args.disable_inline:
+        cmd = ' '.join((cmd,
+                        rcs_utils.get_libdir() + '/libDynAAAliasChecker.a'))
+    cmd = ' '.join((cmd, '-o', args.prog + '.ac'))
     linking_flags = dynaa_utils.get_linking_flags(args.prog)
-    cmd = string.join((cmd, string.join(linking_flags)))
+    cmd = ' '.join((cmd, ' '.join(linking_flags)))
     rcs_utils.invoke(cmd)
