3071dc99324c03960f5ef797534cd50324434ab2
Henry Hu <henry.hu.sh@gmail.com>
2012-11-05 17:27:18 -0500

    count inter procedure ptrs in log_dedup

--- tools/dynaa_log_dedup.py
+++ tools/dynaa_log_dedup.py
@@ -8,6 +8,8 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('input_files', metavar='<input file>', type=str, nargs='+',
     help='log file to be analyzed')
+parser.add_argument('--count-interp-ptr', help = 'count inter-procedure pointer',
+        default = False, action = 'store_true')
 
 args = parser.parse_args()
 logging.basicConfig(level=logging.DEBUG)
@@ -16,6 +18,13 @@ logger.setLevel(logging.DEBUG)
 
 missing_reg = re.compile('.*Missing alias:.*')
 ptrinfo_reg = re.compile('.*\[(\d+)\](.*)$')
+
+ptrinfo_func_reg = re.compile('.*\[(\d+)\]\s+([^ ]+):\s+([^ ]+)\s+=\s+(.*)$')
+GLOBAL_ID = 0
+both_global_count = 0
+one_global_count = 0
+same_func_count = 0
+diff_func_count = 0
 
 for input_file in args.input_files:
     logger.info('Processing %s' % input_file)
@@ -29,6 +38,7 @@ for input_file in args.input_files:
             if (match_ret):
                 got_ptr = 0
                 ptrinfo = []
+                ptrfunc = []
                 while (True):
                     newline = logf.readline()
                     if (newline == ''):
@@ -44,11 +54,41 @@ for input_file in args.input_files:
                         value_desc_db[value_id] = value_desc
                         ptrinfo += [value_id]
 
+                        if (args.count_interp_ptr):
+                            ptrfunc_ret = ptrinfo_func_reg.match(newline)
+                            if (ptrfunc_ret):
+                                func = ptrfunc_ret.group(2)
+                                vname = ptrfunc_ret.group(3)
+                                vsrc = ptrfunc_ret.group(4)
+                                ptrfunc += [func]
+                            else:
+                                # no info? assume global
+                                ptrfunc += [GLOBAL_ID]
                         if (got_ptr == 2):
                             break
 
 #                print "Missing alias:", ptrinfo
                 missing_db.add(frozenset(ptrinfo))
+                if (args.count_interp_ptr):
+                    both_global = False
+                    one_global = False
+                    if (ptrfunc[0] == GLOBAL_ID):
+                        if (ptrfunc[1] == GLOBAL_ID):
+                            both_global = True
+                        else:
+                            one_global = True
+                    else:
+                        if (ptrfunc[1] == GLOBAL_ID):
+                            one_global = True
+                    if (both_global):
+                        both_global_count += 1
+                    elif (one_global):
+                        one_global_count += 1
+                    else:
+                        if (ptrfunc[0] == ptrfunc[1]):
+                            same_func_count += 1
+                        else:
+                            diff_func_count += 1
 
         for pair in missing_db:
             print "Missing alias:"
@@ -60,5 +100,10 @@ for input_file in args.input_files:
                     print "[%d]%s" % (elem, value_desc_db[elem])
                     print "[%d]%s" % (elem, value_desc_db[elem])
         print "Total missing alias pairs: %d" % (len(missing_db))
+        if (args.count_interp_ptr):
+            print "Global pairs: %d" % both_global_count
+            print "Global and local pairs: %d" % one_global_count
+            print "Intra-procedure pairs: %d" % same_func_count
+            print "Inter-procedure pairs: %d" % diff_func_count
 
 
