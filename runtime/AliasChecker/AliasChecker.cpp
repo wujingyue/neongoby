85dd6560c61e107990ecb90be43f7b173c985553
Jingyue Wu <jingyue@cs.columbia.edu>
2012-11-04 21:25:28 -0500

    Grab a lock when reporting bugs in online mode

--- runtime/AliasChecker/AliasChecker.cpp
+++ runtime/AliasChecker/AliasChecker.cpp
@@ -35,14 +35,16 @@ public:
 static queue<void*> FreeQueue, DeleteQueue, DeleteArrayQueue;
 static unsigned FreeTriggerLimit = 20000;
 static unsigned FreeCount = 10000;
-static SpinLock FreeLock;
+static SpinLock FreeLock, ReportLock;
 
 typedef void (*FreeFuncType)(void *Arg);
 extern "C" void _ZdlPv(void *MemBlock);
 extern "C" void _ZdaPv(void *Array);
 
 extern "C" void ReportMissingAlias(unsigned VIDOfP, unsigned VIDOfQ, void *V) {
+  ReportLock.Lock();
   fprintf(stderr, "Missing alias:\n[%u]\n[%u]\n", VIDOfP, VIDOfQ);
+  ReportLock.Unlock();
 }
 
 extern "C" void SilenceMissingAlias(unsigned VIDOfP, unsigned VIDOfQ, void *V) {
